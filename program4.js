b050b3a43e4384de9dea67ded279d19586467b5f
DanteMaycry <1013222027@qq.com>
2017-11-13 21:10:52 +0800

    文件名过滤

---
+++ program4.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var filterWord = '.'+process.argv[3];
+var result = [];
+fs.readdir(process.argv[2],function(err,list){
+  if(err){
+    return console.log(err);
+  }else{
+    list.forEach(function(filename){
+      if(filename.indexOf(filterWord)>0){
+        result.push(filename);
+      }
+    });
+    var aa = result.join("\n");
+    console.log(aa);
+  }
+});
+
+// var fs = require('fs');
+// var path = require('path');
+
+// var folder = process.argv[2]
+// var ext = '.' + process.argv[3]
+
+// fs.readdir(folder, function (err, files) {
+//   if (err) return console.error(err)
+//   files.forEach(function (file) {
+//     if (path.extname(file) === ext) {
+//       console.log(file);
+//     }
+//   });
+// });
