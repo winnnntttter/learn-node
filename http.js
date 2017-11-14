5349c9b9f7e44da000bf24796fd3742b991010d8
DanteMaycry <1013222027@qq.com>
2017-11-14 22:19:16 +0800

    http

---
+++ http.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var url = process.argv[2];
+http.get(url,function(response){
+  response.setEncoding("utf8");
+  response.on("data", function (data) {
+    console.log(data);
+    // data.forEach(function(dt){
+    //   console.log(dt);
+    // });
+  });
+});
+
+// var http = require('http')
+
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8');
+//   response.on('data', console.log);
+//   response.on('error', console.error);
+// }).on('error', console.error);
--- program2.js
+++ program2.js
@@ -2,3 +2,5 @@ var fs = require('fs');
 var obj = fs.readFileSync(process.argv[2]);
 var result = obj.toString().split("\n").length-1;
 console.log(result);
+
+//node program2.js program.js读取文件
