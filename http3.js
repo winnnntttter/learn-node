f55a649d07ae115483e199f1641f9a1559043501
DanteMaycry <1013222027@qq.com>
2017-11-15 23:06:48 +0800

    修改溢出错误

--- http3.js
+++ http3.js
@@ -9,7 +9,7 @@ function printResult(){
   }
 }
 function httpGet(index){
-  http.get(process.argv[2+index],function(respnse){
+  http.get(process.argv[2+index],function(response){
     response.pipe(bl(function(err,data){
       if(err) return console.error(err);
       result[index] = data.toString();
